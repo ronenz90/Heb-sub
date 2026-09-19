fd39dcc8753cc51d2dd86af781a70424aca3d82c
ronenz90 <87617717+ronenz90@users.noreply.github.com>
2026-09-19 22:32:35 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 const manifest = {
   id: 'org.hebrewlivesubs.addon',
   version: '1.2.0',
-  name: 'תרגום עברית חי (Live Hebrew Subs)',
+  name: 'A-HEBSUB By Ronen.z',
   description: 'לוקח כתוביות אנגלית קיימות ומתרגם אותן לעברית תוך כדי צפייה',
   logo: 'https://em-content.zobj.net/source/microsoft-teams/363/flag-israel_1f1ee-1f1f1.png',
   resources: ['subtitles'],
